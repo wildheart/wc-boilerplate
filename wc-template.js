fea3ebbac7e5e6062cb117be56e058b6f3ed8b92
wildheart <boxbit@gmail.com>
2020-01-18 10:36:30 -0500

    Create wc-template.js

---
+++ wc-template.js
@@ -0,0 +1,31 @@
+const tagName = 'wc-template'
+const template = document.createElement('template')
+const css = `
+<style>
+
+</style>
+`
+const html = ''
+template.innerHTML = `${css}${html}`
+class Template extends HTMLElement {
+  constructor() {
+		super()
+	}
+  connectedCallback() {
+    if (!this.shadowRoot) {
+      this.attachShadow({mode: 'open'});
+			this.shadowRoot.appendChild(template.content.cloneNode(true))
+    }
+  }
+  static get observedAttributes() {
+    return ["style"]
+  }
+  set style(value) {
+    this.setAttribute('style', value)
+  }
+  attributeChangedCallback(name, oldVal, newVal) {
+		this[name] = newVal
+  }
+}
+const register = () => customElements.define(tagName, Template);
+window.WebComponents ? window.WebComponents.waitFor(register) : register()
